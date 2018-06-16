857acd1fbe12efccef87782ff441ef2fa06b321b
Mark Mckillion <markmckillion@gmail.com>
2018-06-16 18:35:50 +0100

    Sorted unique domains with counts

--- src/customerimporter/customerimporter.go
+++ src/customerimporter/customerimporter.go
@@ -1,8 +1,3 @@
-// Package customerimporter reads from the given customers.csv file and returns a
-// sorted data structure of your choice of email domains along with the number
-// of customers with e-mail addresses for each domain.
-// Any errors should be logged (or handled). Performance matters (this is only ~3k lines, but *could*
-// be 1m lines or run on a small machine).
 package customerimporter
 
 import (
@@ -11,9 +6,11 @@ import (
 	"io"
 	"log"
 	"os"
+	"sort"
+	"strings"
 )
 
-// Person Comment
+// Person - struct to parse each row of the CSV
 type Person struct {
 	Firstname string
 	Lastname  string
@@ -22,10 +19,9 @@ type Person struct {
 	IPAddress string
 }
 
-// GetPeople Comment
+// GetPeople - Returns a list of people from the CSV
 func GetPeople(fileName string) []Person {
-	csvFile, _ := os.Open(fileName)
-	reader := csv.NewReader(bufio.NewReader(csvFile))
+	reader := getFileReader(fileName)
 	var people []Person
 	for {
 		line, error := reader.Read()
@@ -43,4 +39,111 @@ func GetPeople(fileName string) []Person {
 		})
 	}
 	return people
+}
+
+// GetEmails - Returns a list of emails from the CSV
+func GetEmails(fileName string) []string {
+	reader := getFileReader(fileName)
+	var string []string
+	for {
+		line, error := reader.Read()
+		if error == io.EOF {
+			break
+		} else if error != nil {
+			log.Fatal(error)
+		}
+		string = append(string, line[2])
+
+	}
+	return string
+}
+
+// GetDomains - Returns a list of domains from the CSV
+func GetDomains(fileName string) []string {
+	reader := getFileReader(fileName)
+	var domains []string
+	for {
+		line, error := reader.Read()
+		if error == io.EOF {
+			break
+		} else if error != nil {
+			log.Fatal(error)
+		}
+		if line[2] != "" && strings.Contains(line[2], "@") {
+			components := strings.Split(line[2], "@")
+			if components[1] != "" {
+				domains = append(domains, components[1])
+			}
+		} else {
+			log.Println("Invalid email found. Value = " + line[2])
+		}
+	}
+	return domains
+}
+
+// GetUniqueSortedDomains - Returns a list of domains from the CSV
+func GetUniqueSortedDomains(fileName string) []string {
+	reader := getFileReader(fileName)
+	var domains []string
+	for {
+		line, error := reader.Read()
+		if error == io.EOF {
+			break
+		} else if error != nil {
+			log.Fatal(error)
+		}
+		if line[2] != "" && strings.Contains(line[2], "@") {
+			components := strings.Split(line[2], "@")
+			if components[1] != "" {
+				domains = append(domains, components[1])
+			}
+		} else {
+			log.Println("Invalid email found. Value = " + line[2])
+		}
+	}
+	result := removeDuplicatesUnordered(domains)
+	sort.Sort(sort.StringSlice(result))
+	return result
+}
+
+// getFileReader - Returns file reader based off the file name
+func getFileReader(fileName string) *csv.Reader {
+	csvFile, _ := os.Open(fileName)
+	reader := csv.NewReader(bufio.NewReader(csvFile))
+	return reader
+}
+
+func removeDuplicatesUnordered(elements []string) []string {
+	encountered := map[string]bool{}
+
+	// Create a map of all unique elements.
+	for v := range elements {
+		encountered[elements[v]] = true
+	}
+
+	// Place all keys from the map into a slice.
+	result := []string{}
+	for key, _ := range encountered {
+		result = append(result, key)
+	}
+	return result
+}
+
+// DomainCount - Count some
+func DomainCount(list []string) map[string]int {
+
+	duplicateFrequency := make(map[string]int)
+
+	for _, item := range list {
+		// check if the item/element exist in the duplicate_frequency map
+
+		_, exist := duplicateFrequency[item]
+
+		if exist {
+			duplicateFrequency[item]++ // increase counter by 1 if already in the map
+		} else {
+			duplicateFrequency[item] = 1 // else start counting from 1
+		}
+	}
+	return duplicateFrequency
 }
--- src/main/main.go
+++ src/main/main.go
@@ -2,11 +2,44 @@ package main
 
 import (
 	"fmt"
+	"sort"
 
 	"../customerimporter"
 )
 
 func main() {
-	people := customerimporter.GetPeople("../customers.csv")
-	fmt.Println(people)
+	domains := customerimporter.GetDomains("../customers.csv")
+	fmt.Println("Total Length= ", len(domains))
+
+	domainMap := domainCount(domains)
+	fmt.Println("Unique Domains= ", len(domainMap))
+
+	sortedCounts := make([]string, 0, len(domainMap))
+	for k := range domainMap {
+		sortedCounts = append(sortedCounts, k)
+	}
+	sort.Strings(sortedCounts)
+
+	for _, k := range sortedCounts {
+		fmt.Println(k, domainMap[k])
+	}
+}
+
+func domainCount(domains []string) map[string]int {
+
+	domainFrequency := make(map[string]int)
+
+	for _, item := range domains {
+		// check if the item/element exist in the domainFrequency map
+
+		_, exist := domainFrequency[item]
+
+		if exist {
+			domainFrequency[item]++ // increase counter by 1 if already in the map
+		} else {
+			domainFrequency[item] = 1 // else start counting from 1
+		}
+	}
+
+	return domainFrequency
 }
